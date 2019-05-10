6a2f03b90a6aadb97303cadd7ace713c5480ffb4
Jack Atherton <atherton.lawrence@gmail.com>
2019-05-10 14:16:22 -0700

    working part 1 seedling communication

--- Assets/Scripts/ModalBarClock.cs
+++ Assets/Scripts/ModalBarClock.cs
@@ -7,6 +7,8 @@ public class ModalBarClock : MonoBehaviour
     public OSCHeadBroadcaster myOSC;
     private ChuckSubInstance myChuck;
     public string[] myArpeggio;
+
+    public string[] myModalNotesPart1;
     bool haveInitChuck = false;
 
     // Start is called before the first frame update
@@ -27,7 +29,43 @@ public class ModalBarClock : MonoBehaviour
 
     void StartChuck()
     {
+        // part 1: advertise the next note   
         myChuck.RunCode( myOSC.GenerateChucKCode( "vrSays", "vrHear" ) + string.Format( @"
+            [{0}] @=> int myArpeggio[];
+            int myCurrentNote;
+
+            fun void ListenForPlayedNotes()
+            {{
+                vrHear.event( ""/part1/playedSeedlingNote"", ""i"" ) @=> OscEvent someonePlayedANote;
+
+                while( true )
+                {{
+                    someonePlayedANote => now;
+                    while( someonePlayedANote.nextMsg() != 0 )
+                    {{
+                        // ignore argument
+                        someonePlayedANote.getInt();
+
+                        ( myCurrentNote + 1 ) % myArpeggio.size() => myCurrentNote;
+                    }}
+                }}
+            }}
+            spork ~ ListenForPlayedNotes();
+
+            while( true )
+            {{
+                for( int i; i < vrSays.size(); i++ )
+                {{
+                    // send out the next note someone should play
+                    vrSays[i].startMsg( ""/part1/nextSeedlingNote"", ""f"" );
+                    vrSays[i].addFloat( myArpeggio[myCurrentNote] );
+                }}
+                10::ms => now;
+            }}
+        ", string.Join( ", ", myModalNotesPart1 ) ) );
+
+        // regular modal clock, to be used in part 2
+        /* myChuck.RunCode( myOSC.GenerateChucKCode( "vrSays", "vrHear" ) + string.Format( @"
             [{0}] @=> int myArpeggio[];
             0 => int currentReceiver;
 
@@ -55,5 +93,8 @@ public class ModalBarClock : MonoBehaviour
                 }}
             }}
         ", string.Join( ", ", myArpeggio ) ) );
+        */
+
+        
     }
 }
