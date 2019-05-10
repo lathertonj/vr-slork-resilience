be3a0b7523e58642664cb3a93ca16cd1a2843588
Jack Atherton <atherton.lawrence@gmail.com>
2019-05-10 14:44:24 -0700

    sending modal notes for part 1 working

--- Assets/Scripts/ModalBarClock.cs
+++ Assets/Scripts/ModalBarClock.cs
@@ -9,6 +9,8 @@ public class ModalBarClock : MonoBehaviour
     public string[] myArpeggio;
 
     public string[] myModalNotesPart1;
+    public string[] myAhhNotesPart1a1;
+    public string[] myAhhNotesPart1a2;
     bool haveInitChuck = false;
 
     // Start is called before the first frame update
@@ -32,6 +34,7 @@ public class ModalBarClock : MonoBehaviour
         // part 1: advertise the next note   
         myChuck.RunCode( myOSC.GenerateChucKCode( "vrSays", "vrHear" ) + string.Format( @"
             [{0}] @=> int myArpeggio[];
+            [[{1}], [{2}]] @=> int myAhhNotes[][];
             int myCurrentNote;
 
             fun void ListenForPlayedNotes()
@@ -59,10 +62,26 @@ public class ModalBarClock : MonoBehaviour
                     // send out the next note someone should play
                     vrSays[i].startMsg( ""/part1/nextSeedlingNote"", ""f"" );
                     vrSays[i].addFloat( myArpeggio[myCurrentNote] );
+
+                    // send out the current ahh chord notes
+                    vrSays[i].startMsg( ""/part1/ahhNotes"", ""f,f,f,f,f,f,f,f"" );
+                    vrSays[i].addFloat( myAhhNotes[0][0] );
+                    vrSays[i].addFloat( myAhhNotes[0][1] );
+                    vrSays[i].addFloat( myAhhNotes[0][2] );
+                    vrSays[i].addFloat( myAhhNotes[0][3] );
+                    vrSays[i].addFloat( myAhhNotes[1][0] );
+                    vrSays[i].addFloat( myAhhNotes[1][1] );
+                    vrSays[i].addFloat( myAhhNotes[1][2] );
+                    vrSays[i].addFloat( myAhhNotes[1][3] );
+
                 }}
                 10::ms => now;
             }}
-        ", string.Join( ", ", myModalNotesPart1 ) ) );
+        ", 
+            string.Join( ", ", myModalNotesPart1 ), 
+            string.Join( ", ", myAhhNotesPart1a1 ),
+            string.Join( ", ", myAhhNotesPart1a2 )
+        ) );
 
         // regular modal clock, to be used in part 2
         /* myChuck.RunCode( myOSC.GenerateChucKCode( "vrSays", "vrHear" ) + string.Format( @"
