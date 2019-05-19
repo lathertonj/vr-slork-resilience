d331e2c06d616bee11545e8c73ce3c8769ffba99
Jack Atherton <atherton.lawrence@gmail.com>
2019-05-19 16:12:03 -0700

    after a few seedling jumps, move the entire room up too to hasten the process of upwardsing

---
+++ Assets/Scripts/Part1MoveRoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part1MoveRoom : MonoBehaviour
+{
+    public ChuckSubInstance theChuck;
+    private int numSeedlingsLaunched = 0;
+    private Vector3 currentLocation, goalLocation;
+    private float locationSlew = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<ChuckEventListener>().ListenForEvent( theChuck, "part1SeedlingNotePlayed", RespondToSeedlingLaunch );
+        currentLocation = goalLocation = transform.position;
+    }
+
+    void RespondToSeedlingLaunch()
+    {
+        numSeedlingsLaunched++;
+        if( numSeedlingsLaunched >= 5 )
+        {
+            goalLocation += 0.6f * Vector3.up;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentLocation += locationSlew * Time.deltaTime * ( goalLocation - currentLocation );
+        transform.position = currentLocation;
+    }
+}
